e88d7eca76b4ff109901bda86192efa515536d78
henshaw rowland <henshawrowland@yahoo.com>
2017-01-08 17:03:43 +0100

    Added second test case in modified test file

--- tests.js
+++ tests.js
@@ -1,19 +1,21 @@
 // testing code goes here
 
-'use strict'
+//'use strict'
 
 var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 var should = chai.should();
 
-var myApp = require('getPrimes.js');
+var getPrimes = require('getPrimes');
 
 describe("Test for Proper functionality", function() {
   it("should return [ 2, 3, 5, 7, 11, 13, 17, 19 ] as prime numbers for input of 20", function() {
-    assert(myApp.getPrimes(20) == [ 2, 3, 5, 7, 11, 13, 17, 19 ]);
+    assert(getPrimes(20) == [ 2, 3, 5, 7, 11, 13, 17, 19 ]);
   });
-  
+  it("should return [ 2, 3, 5, 7 ] as prime numbers for input of 10", function() {
+    assert(getPrimes(20) == [ 2, 3, 5, 7 ]);
+  });
 });
 
 
