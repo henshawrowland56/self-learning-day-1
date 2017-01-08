f994aba560b8242c8b5a49a9d870ceb49f0046d3
henshaw rowland <henshawrowland@yahoo.com>
2017-01-08 15:24:14 +0100

    Added getPrimes.js file

---
+++ getPrimes.js
@@ -0,0 +1,21 @@
+function getPrimes(n) {
+	function isPrime(n) {
+		if (n ===2) {return true}
+		else if(n <= 1 || n % 2 === 0){
+			return false
+		}
+		for(var i = 3; i <= Math.sqrt(n); i++){
+			if(n % i === 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	var result = [];
+	for(var i = 0; i<=n; i++){
+		if(isPrime(i)){
+			result.push(i);
+		}
+	}
+	return result;
+}
